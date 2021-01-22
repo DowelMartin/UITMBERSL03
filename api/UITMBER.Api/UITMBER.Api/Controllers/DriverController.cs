fe26f60d75b9cbef150a0bcc93f5de49603cd9e2
LipaMar <48134943+LipaMar@users.noreply.github.com>
2021-01-22 11:04:16 +0100

    Create DriverController.cs

---
+++ api/UITMBER.Api/UITMBER.Api/Controllers/DriverController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UITMBER.Api.Data;
+using UITMBER.Api.DataModels;
+using UITMBER.Api.Repositories.Drivers;
+using UITMBER.Api.Repositories.Drivers.Dto;
+
+namespace UITMBER.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class DriverController : ControllerBase
+    {
+        private readonly IDriverRepository _driverRepository;
+
+        public DriverController(IDriverRepository driverRepository)
+        {
+            _driverRepository = driverRepository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<DriverDto>> GetNerbyDriveresAsync(double latitude, double longitude, double maxDistance=1) {
+
+            List<DriverDto> drivers = await _driverRepository.GetDrivers();
+            return drivers.Where(driver => CalculateDistance(latitude, longitude, driver.Lat, driver.Long) <= maxDistance);
+        }
+        private double CalculateDistance(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+        }
+
+        [HttpGet]
+        public async Task<DriverDto> GetProfile(int id)
+        {
+            List<DriverDto> drivers = await _driverRepository.GetDrivers();
+            return drivers.Where(driver => driver.Id==id).FirstOrDefault();
+        }
+    }
+}
