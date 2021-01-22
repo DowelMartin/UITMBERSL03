3ae3dbd1678ac12b151c8edaaab41ca59be011c7
LipaMar <48134943+LipaMar@users.noreply.github.com>
2021-01-22 23:38:50 +0100

    Update Startup.cs

--- api/UITMBER.Api/UITMBER.Api/Startup.cs
+++ api/UITMBER.Api/UITMBER.Api/Startup.cs
@@ -19,6 +19,7 @@ using UITMBER.Api.Configuration;
 using UITMBER.Api.Data;
 using UITMBER.Api.Repositories.Auth;
 using UITMBER.Api.Repositories.Cars;
+using UITMBER.Api.Repositories.Locations;
 
 namespace UITMBER.Api
 {
@@ -79,7 +80,8 @@ namespace UITMBER.Api
 
             services.AddTransient<IAuthenticationRepository, AuthenticationRepository>();
             services.AddTransient<ICarRepository, CarRepository>();
-            
+            services.AddTransient<ILocationRepository, LocationRepository>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
